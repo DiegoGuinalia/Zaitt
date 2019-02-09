fa9ddd0f8b0deb7832d31861269cbd3675499391
diegoguinalia <diego.rruy@hotmail.com>
2019-02-09 23:33:12 +0000

    add webhook and adjust views

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::IssuesController < Api::V1::BaseController
- 
+  skip_before_action :verify_authenticity_token
+  
   def index
     issues = Issue.all.order(:created_at)
     render json: issues.to_json(:only => [
@@ -18,6 +19,15 @@ class Api::V1::IssuesController < Api::V1::BaseController
   end
   
   def create
-  end
+    parsed_request = JSON.parse(request.body.read)
     
+    issue = Issue.new(
+      action: parsed_request["action"],
+      repository_id: parsed_request["issue"]["id"],
+      repository_name: parsed_request["repository"]["name"],
+      owner_name: parsed_request["repository"]["owner"]["login"]
+    )
+    
+    render json: { data: "#{issue.to_json}" }, status: issue.save ? 201 : 400
+  end
 end
